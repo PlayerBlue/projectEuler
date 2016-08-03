707d840ad2a9d323e321331e2ea72edfc318f89d
PlayerBlue <epikadiblue1@gmail.com>
2016-08-03 18:45:15 +0200

    Add files via upload

---
+++ project-euler.js
@@ -0,0 +1,60 @@
+function multipliesOf3And5s1(limit) {
+	var sum = 0;
+	for (var i = 0; i < limit; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+function multipliesOf3And5s2(limit) {
+	var sum = 0, limit3 = limit / 3, limit5 = limit / 5, x = 0;
+	for (var i = 1; i < limit3; i++) {
+		x = i * 5;
+		if (i < limit5 && x % 3 !== 0) {
+			sum += x;
+		}
+		sum += i * 3;
+	}
+	return sum;
+}
+
+function evenFibonacciSum(limit) {
+	var n1 = 1, n2 = 2, sum = 0;
+	while (n2 <= limit) {
+		if (n2 % 2 === 0) {
+			sum += n2;
+		}
+		n2 += n1;
+		n1 = n2 - n1;
+	}
+	return sum;
+}
+
+
+function largestPrimeNum(number) {
+	var max1 = Math.sqrt(number), maxPrime = 1;
+	for (var i = 2; i <= max1; i++) {
+		if (number % i === 0) {
+			var primeCheck = number / i, max2 = Math.sqrt(primeCheck), max3 = Math.sqrt(i);
+			for (var j = 2; j <= max2 && primeCheck % j !== 0; j++);
+
+			if (j > max2) {
+				return primeCheck;
+			}
+			
+			for (var k = 2; k <= max3 && i % k !== 0; k++);
+			if (k > max3) {
+				maxPrime = i;
+			}
+		}
+	}
+	if (maxPrime > 1) {
+				return maxPrime;
+			}
+	return number;
+}
+}
+
+largestPrimeNum(600851475143);
