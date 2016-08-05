f8f77b47e269f1f2cbbe6b6a06e07f1201cf5f41
PlayerBlue <epikadiblue1@gmail.com>
2016-08-05 16:49:00 +0200

    Update project-euler.js

--- project-euler.js
+++ project-euler.js
@@ -20,6 +20,19 @@ function multipliesOf3And5s2(limit) {
 	return sum;
 }
 
+function multipliesOf3And5s2(numbers) {
+	numbers[3] = numbers[1] * numbers[2];
+	var max = [];
+	var sum = 0;
+	for (var i = 0; i < 3; i++) {
+		max[i] = numbers[0] / numbers[i + 1];
+		max[i] = (max[i] % i === 0) ? max[i] - 1 : Math.floor(max[i]);
+		var sum1 = max[i] * (max[i] + 1) / 2 * numbers[i + 1];
+		sum += (i < 2) ? sum1 : -sum1;
+	}
+	return sum;
+}
+
 function evenFibonacciSum(limit) {
 	var n1 = 1, n2 = 2, sum = 0;
 	while (n2 <= limit) {
