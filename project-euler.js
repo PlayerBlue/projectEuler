2cab79e45ec5f8cc96645cf64ff89aa1e137a039
PlayerBlue <epikadiblue1@gmail.com>
2016-08-06 14:55:59 +0200

    Update project-euler.js

--- project-euler.js
+++ project-euler.js
@@ -1,25 +1,3 @@
-function multipliesOf3And5s1(limit) {
-	var sum = 0;
-	for (var i = 0; i < limit; i++) {
-		if (i % 3 === 0 || i % 5 === 0) {
-			sum += i;
-		}
-	}
-	return sum;
-}
-
-function multipliesOf3And5s2(limit) {
-	var sum = 0, limit3 = limit / 3, limit5 = limit / 5, x = 0;
-	for (var i = 1; i < limit3; i++) {
-		x = i * 5;
-		if (i < limit5 && x % 3 !== 0) {
-			sum += x;
-		}
-		sum += i * 3;
-	}
-	return sum;
-}
-
 function multipliesOf3And5s3(numbers) {
 	numbers[3] = numbers[1] * numbers[2];
 	var max = [];
@@ -45,25 +23,8 @@ function evenFibonacciSum(limit) {
 	return sum;
 }
 
-function largestPrimeNums1(number) {
-	var max1 = Math.sqrt(number), maxPrime = number;
-	for (var i = 2; i <= max1; i++) {
-		if (number % i === 0) {
-			var primeCheck = number / i, max2 = Math.sqrt(primeCheck), max3 = Math.sqrt(i);
-			for (var j = 2; j <= max2 && primeCheck % j !== 0; j++);
-			if (j > max2) {
-				return primeCheck;
-			}			
-			for (var k = 2; k <= max3 && i % k !== 0; k++);
-			if (k > max3) {
-				maxPrime = i;
-			}
-		}
-	}
-	return maxPrime;
-}
 
-function largestPrimeNums2(number) {
+function largestPrimeNum(number) {
 	while (number % 2 === 0) {
 		number /= 2;
 	}
